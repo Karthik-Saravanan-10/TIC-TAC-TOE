fe2e0de15657f1a9c73eb7785cb6a042ee4c524e
Karthik Saravanan <93970491+Karthik-Saravanan-10@users.noreply.github.com>
2024-02-12 09:50:21 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,45 @@
-block=["-" for i in range(9)]
+blocks=["-" for i in range (9)]
+
+def display_game():
+    print("|",blocks[0],"|",blocks[1],"|",blocks[2],"|")
+    print("|",blocks[3],"|",blocks[4],"|",blocks[5],"|")  
+    print("|",blocks[6],"|",blocks[7],"|",blocks[8],"|")    
 
 player1="x"
-player2="0"
-
-def display():
-    print("|",block[0],"|",block[1],"|",block[2],"|")
-    print("|",block[3],"|",block[4],"|",block[5],"|")
-    print("|",block[6],"|",block[7],"|",block[8],"|")
+player2="o"
 
 def rules(player):
-    conditions=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    conditions=[
+    [0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
     for check in conditions:
-        if 
+        if blocks[check[0]]==player and blocks[check[1]]==player and blocks[check[2]]==player:
+            return 1
+        return 0
 
-display()
+def start_game():
+    display_game()
+    while True:
+        while True:
+            playeroption1=input(f"{player1}  Enter the place[1-9]:")
+            if blocks[int(playeroption1) - 1] == "-":
+                blocks[int(playeroption1) -1]=player1
+                display_game()
+                if rules(player1):
+                    return f'Winner:{player1}'
+                break
+            else: 
+                print("This place already alloted")
+
+        while True:
+            playeroption2=input(f"{player2}  Enter the place[1-9]:")
+            if blocks[int(playeroption2) - 1] == "-":
+                blocks[int(playeroption2) -1]=player2
+                display_game()
+                if rules(player1):
+                    return f'Winner:{player1}'
+                break
+            else: 
+                print("This place already alloted")
+
+
+start_game()
